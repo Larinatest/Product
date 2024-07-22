c270970679f0708c30da93e1e9fdf82de8972683
Evgenia Larina <larsik29@yandex.ru>
2024-07-22 13:30:23 +0300

    Initial commit

--- src/test/java/ShopRepositoryTest.java
+++ src/test/java/ShopRepositoryTest.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class ShopRepositoryTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+
+    @Test
+    public void shouldRemoveExistingProduct() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(1, "Product 1", 100);
+        Product product2 = new Product(2, "Product 2", 200);
+        repo.add(product1);
+        repo.add(product2);
+
+        repo.removeById(1);
+
+        Product[] expected = {product2};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowNotFoundExceptionIfProductNotFound() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(1, "Product 1", 100);
+        Product product2 = new Product(2, "Product 2", 200);
+        repo.add(product1);
+        repo.add(product2);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.removeById(3);
+        });
+    }
 }
